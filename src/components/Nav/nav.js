bbd20d56944f106a3ad1e48b881d7b7b73f14fe3
Annie <annie.yiyunc@gmail.com>
2020-01-21 21:55:49 -0800

    added initial files

---
+++ src/components/Nav/nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-primary">
+      <a className="navbar-brand" href="/">
+        Clicky Game
+      </a>
+    </nav>
+  );
+}
+
+export default Nav;
